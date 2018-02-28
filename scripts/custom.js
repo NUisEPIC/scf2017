4b4683f2a0a1ddb1e51eff103595568e59b91d03
Pranav Dhingra <pranav.dhingra@gmail.com>
2018-02-27 22:00:06 -0600

    Commented out some JS to fix navbar color issue
    Issue was really with the CSS, but seems unnecessary

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,6 +7,8 @@
 
 
 	//jQuery to collapse the navbar on scroll
+	// CSS for top-nave-collapse is a bit wrong, so commenting this out - Not really necessary. --Pranav
+	/*
 	$(window).scroll(function() {
 		if ($(".navbar").offset().top > 50) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -14,6 +16,7 @@
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
 		}
 	});
+	*/
 
 	//jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
